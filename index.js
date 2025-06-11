cbf6dab0910be6abe3c20fa80fde7d4d18129490
Oliver Lomanat <lomanatoliver@gmail.com>
2025-06-11 08:07:03 +0300

    Using objects methods

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+const employee = {
+    name: "Dida",
+    streetAddress: 30501,
+}
+
+function updateEmployeeWithKeyAndValue(obj,key,value) {
+    const newObj = {...obj}
+    newObj[key] = value
+    return newObj
+}
+const employee2 = updateEmployeeWithKeyAndValue(employee,"lab",2)
+console.log(employee2)
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value) {
+    obj[key] = value
+   
+    return obj
+}
+const employee3 = destructivelyUpdateEmployeeWithKeyAndValue(employee,"name","Dida")
+console.log(employee3)
+
+
+function deleteFromEmployeeByKey(obj,key) {
+    const newObj = {...obj}
+
+    delete newObj[key]
+    return newObj
+}
+console.log(deleteFromEmployeeByKey(employee,"name"))
+
+function destructivelyDeleteFromEmployeeByKey(obj,key) {
+    delete obj[key]
+    return obj
+}
+console.log(destructivelyDeleteFromEmployeeByKey(employee,"name"))
